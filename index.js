7a2e3667cfe2fbd7bdb0812087df808f9fb3b1d1
FifiTheBulldog <fifithebulldog@outlook.com>
2022-04-23 12:27:15 -0400

    Rewrite to use repl module and ES imports

--- index.js
+++ index.js
@@ -1,69 +1,84 @@
-const { hostname } = require("os");
-const { createInterface } = require("readline");
-const { WebSocketServer } = require("ws");
-const { red, bold: { blue } } = require("ansi-colors");
+import { hostname } from "os";
+import repl from "repl";
+import { WebSocketServer } from "ws";
+import colors from "ansi-colors";
+const { red, bold: { blue } } = colors;
 
-// Keep track of whether we need to log an additional empty line
 let isPrompting = false;
 
-
 // Utility functions for more visually pleasing logs
-const colorLog = (data, source, color) => {
-  // Get out of user input area if prompt is currently being shown
-  // Then log
-  console.log((isPrompting ? "\n" : "") + color(`[${source}] `) + data);
-}
+// Get out of user input area first if prompt is currently being shown
+const colorize = (data, source, color) => color(`[${source}] `) + data;
+const safeLog = (data) => console.log((isPrompting ? "\n" : "") + data);
 
-const debuggerLog = (data) => colorLog(data, "Debugger", blue);
+const discordColorize = (data) => colorize(JSON.parse(data).message, "Discord", blue);
+const discordLog = (data) => safeLog(discordColorize(data));
 
-const debuggerError = (err) => {
-  colorLog(red("Error"), "Debugger", red.bold);
+const debuggerColorize = (data) => colorize(data, "Debugger", blue);
+const debuggerLog = (data) => safeLog(debuggerColorize(data));
+const debuggerError = (err, isReturning) => {
+  safeLog(colorize(red("Error"), "Debugger", red.bold));
+  if (isReturning) {
+    return err;
+  }
   console.error(err);
 }
 
 
-// Create REPL UI
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// Display welcome message and basic instructions
+console.log("Welcome to the unofficial Enmity debugger.")
+console.log("Press Ctrl+C to exit.")
+console.log(`Connect to this debugger from Discord on your iOS device
+by typing the following slash command in the chat box:
 
-const prompt = () => {
-  isPrompting = true;
-  rl.prompt();
-}
+  /websocket host:${hostname()}:9090
+`);
 
-rl.on("line", (input) => {
-  try {
-    isPrompting = false;
-    globalWs.send(input);
-  } catch (e) {
-    debuggerError(e);
-    prompt();
-  }
-});
-
-rl.on("close", () => {
-  debuggerLog("Closing debugger");
-  process.exit();
-});
-
-
-// Create websocket
-let globalWs;
+// Create websocket server and REPL, and wait for connection
 const wss = new WebSocketServer({ port: 9090 });
-
-// Add server connection handler and websocket event handlers
 wss.on("connection", (ws) => {
   debuggerLog("Connected to Discord over websocket, starting debug session");
 
+  isPrompting = false; // REPL hasn't been created yet
+  let finishCallback;
+
+  // Handle logs returned from Discord client via the websocket
   ws.on("message", (data) => {
     try {
-      colorLog(JSON.parse(data).message, "Discord", blue);
+      if (finishCallback) {
+        finishCallback(null, data);
+        finishCallback = undefined;
+      } else {
+        discordLog(data);
+      }
     } catch (e) {
-      debuggerError(e);
+      debuggerError(e, false);
     }
-    prompt();
+    isPrompting = true;
+    rl.displayPrompt();
+  });
+
+  // Create the REPL
+  const rl = repl.start({
+    eval: (input, ctx, filename, cb) => {
+      try {
+        isPrompting = false;
+        ws.send(input);
+        finishCallback = cb;
+      } catch (e) {
+        cb(e);
+      }
+    },
+    writer: (data) => {
+      return (data instanceof Error) ? debuggerError(data, true) : discordColorize(data);
+    }
+  });
+
+  isPrompting = true; // Now the REPL exists and is prompting the user for input
+  
+  rl.on("exit", () => {
+    debuggerLog("Closing debugger");
+    process.exit();
   });
 
   ws.on("close", () => {
@@ -71,19 +86,6 @@ wss.on("connection", (ws) => {
     isPrompting = false;
     rl.close();
   });
-
-  globalWs = ws;
 });
-
-
-// Display welcome message and basic instructions
-console.log(`Welcome to the unofficial Enmity debugger.
-Press Ctrl+C at any time to exit.
-
-Connect to this debugger from Discord on your iOS device
-by typing the following slash command in the chat box:
-
-  /websocket host:${hostname()}:9090
-`);
 
 debuggerLog("Ready to connect");
